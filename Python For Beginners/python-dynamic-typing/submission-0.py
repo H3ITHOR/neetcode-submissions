d9d82fdac10838640f005aebc8b03042367bb99e
Heitor Felipe Marques Barros <125290781+H3ITHOR@users.noreply.github.com>
2026-06-18 12:08:44 -0300

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,19 @@
+variable = 12
+
+print(type(variable))
+
+variable = 12.1
+
+print(type(variable))
+
+variable = True
+
+print(type(variable))
+
+variable = "Test"
+
+print(type(variable))
+
+variable = ["Test", 12, 12.1]
+
+print(type(variable))
