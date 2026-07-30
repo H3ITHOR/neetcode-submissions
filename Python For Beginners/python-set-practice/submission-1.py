455bbf82b9d785359926ceec3067db76f40567bf
Heitor Felipe Marques Barros <125290781+H3ITHOR@users.noreply.github.com>
2026-07-29 22:25:13 -0300

    Add: python-set-practice - submission-1

---
+++ Python For Beginners/python-set-practice/submission-1.py
@@ -0,0 +1,20 @@
+from typing import List
+
+def contains_duplicate(words: List[str]) -> bool:
+    # 1° way:
+    # if(len(words) > len(set(words))):
+    #     return True
+    # return False
+
+    # 2° way:
+    sw = sorted(words)
+    for i in range(len(sw)-1):
+        if sw[i] == sw[i+1]:
+            return True
+    return False
+
+# do not modify code below this line
+print(contains_duplicate(["hello", "world", "hello"]))
+print(contains_duplicate(["hello", "world", "i", "am", "great"]))
+print(contains_duplicate(["hello", "hello", "hello"]))
+print(contains_duplicate(["Hello", "hellooo", "hello"]))
